c5d5b9a2008970cf99ac6323c20b4ed7767e2138
ltummalapal <Lahari_tummalapalli@intuit.com>
2023-04-23 09:35:38 +0530

    Lambdas with Parameters

--- src/main/java/com/java8features/understandinglambdas/UnderstandingLambdasApplication.java
+++ src/main/java/com/java8features/understandinglambdas/UnderstandingLambdasApplication.java
@@ -3,15 +3,20 @@ package com.java8features.understandinglambdas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 interface Calculator{
-    void switchOn();
+    int sum (int a, int b);
 }
 @SpringBootApplication
 public class UnderstandingLambdasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UnderstandingLambdasApplication.class, args);
-        Calculator calculator = () -> {System.out.println("Turn on the calculator");};
-        calculator.switchOn();
+//        Calculator calculator = () -> {System.out.println("Turn on the calculator");};
+//        calculator.switchOn();
+            Calculator calculator = ( firstNum, secondNum) -> {
+                int sum = (firstNum + secondNum);
+                return sum;
+            };
+            System.out.println("The sum of the two numbers is " +calculator.sum(15,30));
     }
 
 }
